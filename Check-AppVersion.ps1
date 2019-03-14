44fba854e5a23879c2750b4ef628926778bf7460
ctrl-freak <bryce.sheehan@gmail.com>
2019-03-14 15:15:11 +0800

    Add files via upload

---
+++ Check-AppVersion.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [Array] $App = @()
+    ,[String] $File
+    ,[Parameter(Mandatory=$true)][String] $APIKey # = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    ,[String] $APIStore = '/stores/android'
+)
+
+If ($File -ne $null) {
+    $App = (Import-PowerShellDataFile $File).Apps
+}
+
+If ($App.Count -gt 0) {
+    . .\AppMonsta.ps1
+
+    $AppMonsta = [AppMonsta]@{
+        APIKey = $APIKey
+        APIStore = $APIStore
+    }
+
+    $AppDetails = @()
+
+    Foreach ($AppID in $App) {
+        $AppDetails += $AppMonsta.GetAppDetails($AppID)
+    }
+
+    $AppDetails | Select app_name,id,version
+} else {
+    Throw 'Please supply an Android App ID'
+}
